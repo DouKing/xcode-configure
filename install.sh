d2be25462f41e800ebc2c3e3b1cc8235a2566787
DouKing <wyk8916@gmail.com>
2024-07-09 11:04:31 +0800

    Delete fonts, use git repo

--- install.sh
+++ install.sh
@@ -3,14 +3,11 @@
 DIR=~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 mkdir -p $DIR
 cp ./ColorTheme/*.xccolortheme $DIR
-
 cp ./IDETemplateMacros.plist ~/Library/Developer/Xcode/UserData/IDETemplateMacros.plist
-
-cp -R ./font ~/Library/Fonts/
+git clone git@github.com:DouKing/MyFonts.git ~/Library/Fonts/MyFonts
 
 TEMPLATES_DIR=~/Library/Developer/Xcode/Templates/File\ Templates/
 if [[ ! -d "${TEMPLATES_DIR}" ]]; then
 	mkdir -p "${TEMPLATES_DIR}"
 	cp -R ./DouKing "${TEMPLATES_DIR}"
-	./change-file-header "${TEMPLATES_DIR}"
 fi
